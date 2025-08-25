6ec4cccb185da29f6bc0d62399a8347ca2f5789c
User <sherliz.yakubova@icloud.com>
2025-08-25 10:53:45 +0300

    screen

--- screen.py
+++ screen.py
@@ -3,41 +3,54 @@ import pygame
 from consts import *
 
 
-
-def solider_location(screen,location):
+def draw_solider(screen, location):
     solider_image = pygame.image.load('soldier.png')
     solider_image = pygame.transform.scale(solider_image, (P_SOLIDER_WIDTH, P_SOLIDER_HEIGHT))
     screen.blit(solider_image, location)
-def flag_location(screen,location):
+
+
+def draw_flag(screen, location):
     flag_image = pygame.image.load('flag.png')
     flag_image = pygame.transform.scale(flag_image, (P_FLAG_WIDTH, P_FLAG_HEIGHT))
     screen.blit(flag_image, location)
-def add_grass():
-    pygame.init()
-    screen = pygame.display.set_mode((P_SCREEN_WIDTH, P_SCREEN_HEIGHT))
-    screen.fill(pygame.Color('green'))
+
+
+def draw_grass(screen):
     grass_image = pygame.image.load('grass.png')
     grass_image = pygame.transform.scale(grass_image, (P_GRASS_WIDTH, P_GRASS_HEIGHT))
-    for element in range(P_GRASS_AMOUNT):
+    for _ in range(P_GRASS_AMOUNT):
         x = random.randint(0, P_SCREEN_WIDTH - P_GRASS_WIDTH)
         y = random.randint(0, P_SCREEN_HEIGHT - P_GRASS_HEIGHT)
         screen.blit(grass_image, (x, y))
-    solider_location(screen, (0, 0))
-    flag_x=P_SCREEN_WIDTH - P_FLAG_WIDTH
-    flag_y=P_SCREEN_HEIGHT - P_FLAG_HEIGHT
-    flag_location(screen, (flag_x, flag_y))
-    sent=pygame.font.Font(None, 20)
-    titles=sent.render('Welcome to the flag game.', True, (255, 255, 255))
-    titles2=sent.render('Have fun!', True, (255, 255, 255))
-    screen.blit(titles,(65,15))
-    screen.blit(titles2,(65,25))
+
+
+def draw_text(screen):
+    font = pygame.font.Font(None, 20)
+    title1 = font.render('Welcome to the flag game.', True, (255, 255, 255))
+    title2 = font.render('Have fun!', True, (255, 255, 255))
+    screen.blit(title1, (65, 15))
+    screen.blit(title2, (65, 25))
+
+
+def screen_main():
+    pygame.init()
+    screen = pygame.display.set_mode((P_SCREEN_WIDTH, P_SCREEN_HEIGHT))
+    screen.fill(pygame.Color('green'))
+
+    draw_grass(screen)
+    draw_solider(screen, (0, 0))
+    draw_flag(screen, (P_SCREEN_WIDTH - P_FLAG_WIDTH, P_SCREEN_HEIGHT - P_FLAG_HEIGHT))
+    draw_text(screen)
+
     pygame.display.flip()
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
     pygame.quit()
 
-add_grass()
 
+screen_main()
