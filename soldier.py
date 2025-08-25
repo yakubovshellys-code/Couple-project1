7b21806c37837ea1159b95143db95b4f55c4ba88
ateret789-pixel <ateret789@gmail.com>
2025-08-25 09:25:09 +0300

    commit save soldier

--- soldier.py
+++ soldier.py
@@ -6,13 +6,13 @@ soldier = (0,0)
 
 # Sets tha soldier (body, and legs)
 def soldier_init(game_grid):
-    for height_body in range(soldier[0], consts.HEIGHT_BODY_SOLDIER):
-        for width in range(soldier[1], consts.WIDTH_SOLDIER):
-            game_grid[height_body][width] = consts.BODY_SOLDIER
+    for height_body in range(soldier[0], consts.BODY_SOLDIER_HEIGHT):
+        for width in range(soldier[1], consts.SOLDIER_WIDTH):
+            game_grid[height_body][width] = consts.SOLDIER_BODY
 
-    for height_legs in range(soldier[0] + consts.HEIGHT_BODY_SOLDIER, consts.HEIGHT_SOLDIER):
-        for width in range(soldier[1], consts.WIDTH_SOLDIER):
-            game_grid[height_legs][width] = consts.LEG_SOLDIER
+    for height_legs in range(soldier[0] + consts.BODY_SOLDIER_HEIGHT, consts.SOLDIER_HEIGHT):
+        for width in range(soldier[1], consts.SOLDIER_WIDTH):
+            game_grid[height_legs][width] = consts.SOLDIER_LEG
 
 
 """def if_in_grid():
